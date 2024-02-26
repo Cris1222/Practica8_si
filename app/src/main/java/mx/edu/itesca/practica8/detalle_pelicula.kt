28d34429585bfe62de2e273ca73900d237567689
Cris <Cristiancoronatapia@gmail.com>
2024-02-26 16:41:47 -0700

    uwu

--- app/src/main/java/mx/edu/itesca/practica8/detalle_pelicula.kt
+++ app/src/main/java/mx/edu/itesca/practica8/detalle_pelicula.kt
@@ -1,7 +1,9 @@
 package mx.edu.itesca.practica8
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -13,7 +15,12 @@ class detalle_pelicula : AppCompatActivity() {
         val iv_pelicula_image: ImageView =findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView =findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView =findViewById(R.id.tv_pelicula_desc)
-
+        val tickect: Button=findViewById(R.id.buyTickets)
+        tickect.setOnClickListener {
+            var intent: Intent = Intent( this,
+                SeatSelection::class.java)
+            startActivity(intent)
+        }
         val bundle=intent.extras
         if (bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
